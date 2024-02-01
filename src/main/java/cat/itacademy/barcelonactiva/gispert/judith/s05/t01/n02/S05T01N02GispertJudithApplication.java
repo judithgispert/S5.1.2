4c618fb32587f7e2f17ee90e82ea778e093e005e
judithgispert <jugispert@gmail.com>
2024-02-01 10:35:16 +0100

    Initial commit

---
+++ src/main/java/cat/itacademy/barcelonactiva/gispert/judith/s05/t01/n02/S05T01N02GispertJudithApplication.java
@@ -0,0 +1,13 @@
+package cat.itacademy.barcelonactiva.gispert.judith.s05.t01.n02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S05T01N02GispertJudithApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S05T01N02GispertJudithApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cat/itacademy/barcelonactiva/gispert/judith/s05/t01/n02/S05T01N02GispertJudithApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.itacademy.barcelonactiva.gispert.judith.s05.t01.n02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S05T01N02GispertJudithApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
