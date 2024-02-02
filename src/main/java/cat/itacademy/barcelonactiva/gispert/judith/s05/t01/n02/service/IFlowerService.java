5a4215d0f9aa591e2e0a0d6c4379bd8a12b4ba98
judithgispert <jugispert@gmail.com>
2024-02-02 09:43:49 +0100

    Interface and methods

--- src/main/java/cat/itacademy/barcelonactiva/gispert/judith/s05/t01/n02/service/IFlowerService.java
+++ src/main/java/cat/itacademy/barcelonactiva/gispert/judith/s05/t01/n02/service/IFlowerService.java
@@ -1,4 +1,14 @@
 package cat.itacademy.barcelonactiva.gispert.judith.s05.t01.n02.service;
 
+import cat.itacademy.barcelonactiva.gispert.judith.s05.t01.n02.domain.Flower;
+import cat.itacademy.barcelonactiva.gispert.judith.s05.t01.n02.dto.FlowerDTO;
+
+import java.util.List;
+
 public interface IFlowerService {
+    public void saveFlower(FlowerDTO flowerDTO);
+    public FlowerDTO getFlowerById(int id);
+    public List<FlowerDTO> getFlowers();
+    public FlowerDTO updateFlower(FlowerDTO flowerDTO, int id);
+    public void deleteFlower(int id);
 }
