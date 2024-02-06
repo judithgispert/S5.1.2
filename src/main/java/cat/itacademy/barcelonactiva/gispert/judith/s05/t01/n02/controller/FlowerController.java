db5351ade05d73631074d2a961d25f71c915d7d2
judithgispert <jugispert@gmail.com>
2024-02-06 12:37:10 +0100

    Change request mapping

--- src/main/java/cat/itacademy/barcelonactiva/gispert/judith/s05/t01/n02/controller/FlowerController.java
+++ src/main/java/cat/itacademy/barcelonactiva/gispert/judith/s05/t01/n02/controller/FlowerController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/flower")
+@RequestMapping("/flower")
 public class FlowerController {
     @Autowired
     private FlowerServiceImpl flowerService;
